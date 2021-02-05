b00e7e68ccfe89eb7fbc081c5100416b0cfc8550
Saurabh <saurabhzade703@gmail.com>
2021-02-05 15:38:04 +0530

    update executor

--- executor.sh
+++ executor.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
 echo "Hello World!!!"
-file="$1"
-time file
+file_name="$1"
+if [ $file_name ]
+then 
+	echo "$file_name present"
+else
+	echo "$file_name not present"
+fi
+time $file_name
